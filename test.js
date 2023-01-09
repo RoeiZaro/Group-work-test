b7d774ea19feea02b8b828d527376c1c55051695
o6mer <o6merliraz@gmail.com>
2023-01-09 11:26:44 +0200

    best webstie ever

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log("omer branch");
+const body = document.querySelector("body");
+body.style.backgroundColor = "blue";
+const p = document.querySelector("p");
+p.innerHTML = "fuck nati";
+p.addEventListener("click", (e) => {
+  p.style.color = "red";
+  p.classList.add("big");
+});
